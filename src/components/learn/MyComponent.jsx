35d3f60d9d8296a812d3c1cd1e317aefde8a482e
An Nguyen <annguyen@example.com>
2025-05-19 16:57:08 +0700

    Video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,9 +4,20 @@
 import './style.css'
 
 const MyComponent = () => {
+    // const hoidanit = An; //string
+    // const hoidanit = 25; //number
+    // const hoidanit = true; // boolean
+    // const hoidanit = undefined;
+    // const hoidanit = null;
+    const hoidanit = [1, 2, 3];
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25
+    // };
     return (
         <>
-            <div> An & HoidanIT</div>
+            <div> {JSON.stringify(hoidanit)} & HoidanIT</div>
+            <div>{console.log("An")}</div>
             <div className="child"
                 style={{ borderRadius: "10px" }}
             >child</div>
