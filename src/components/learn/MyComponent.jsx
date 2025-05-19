673e061c47c5f26471717901cb8dbe9bb15493dc
An Nguyen <annguyen@example.com>
2025-05-19 15:54:52 +0700

    Video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+const MyComponent = () => {
+    return (
+        <div> An & HoidanIT</div>
+    );
+}
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,19 @@
+
+const ThirdComponent = () => {
+    return (
+        <div>
+            third component
+        </div>
+    )
+}
+
+
+const SecondComponent = () => {
+    return (
+        <div>
+            second component
+        </div>
+    )
+}
+
+export { SecondComponent, ThirdComponent }
