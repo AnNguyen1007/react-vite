b7c7300616f6097f00d716dfad00a0bb9881e9dc
An Nguyen <annguyen@example.com>
2025-05-19 16:14:31 +0700

    Video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,6 +1,17 @@
+//JSX
+//fragment
+
+import './style.css'
+
 const MyComponent = () => {
     return (
-        <div> An & HoidanIT</div>
+        <>
+            <div> An & HoidanIT</div>
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
+
     );
 }
 
